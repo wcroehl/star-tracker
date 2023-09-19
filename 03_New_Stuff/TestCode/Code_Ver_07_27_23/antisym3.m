a465063d00bad930a9a46fa893ae440fdebc3228
William Roehl <111902639+wcroehl@users.noreply.github.com>
2023-09-19 18:21:04 -0500

    Delete 03_New_Stuff directory

--- 03_New_Stuff/TestCode/Code_Ver_07_27_23/antisym3.m
+++
@@ -1,5 +0,0 @@
-function A = antisym3(vec) 
-A = [      0    vec(3) -vec(2)
-      -vec(3)      0   vec(1)
-       vec(2)  -vec(1)      0  ];
-end
--- 03_New_Stuff/TestCode/Code_Ver_07_31_23/TestCode/cartesian.m
+++
@@ -1,5 +0,0 @@
-function L = cartesian(alpha, delta) 
-% Compute unit vector of star position 
-L = [cos(delta) * cos(alpha) ; % usually on x axis */
-     cos(delta) * sin(alpha) ; % usually on y axis */ 
-     sin(delta) ];  % usually on z axis */ 
