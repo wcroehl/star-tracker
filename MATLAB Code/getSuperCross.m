285234502d6050719aa86c53a53bd993ff7e4dee
Will Roehl <wcroehl@iastate.edu>
2023-11-02 14:11:24 -0500

    returned matrix with opposite values

--- MATLAB Code/getSuperCross.m
+++ MATLAB Code/getSuperCross.m
@@ -4,8 +4,8 @@ function supX = getSuperCross(v)
 % Source: Aero 540, Aero 575, equation 3.149 in Crassidis, and
 % http://en.wikipedia.org/wiki/Cross_product#Conversion_to_matrix_multiplication
 
-supX = [0    -v(3)  v(2);
-        v(3)  0    -v(1);
-       -v(2)  v(1)  0];
+supX = [0    v(3)  -v(2);
+        -v(3)  0    v(1);
+       v(2)  -v(1)  0];
    
 end
