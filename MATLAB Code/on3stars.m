cbe598d2a53483e3093e3d4fe9e773c490c47af6
Will Roehl <wcroehl@iastate.edu>
2024-02-02 12:18:19 -0600

    wrong value for m

--- MATLAB Code/on3stars.m
+++ MATLAB Code/on3stars.m
@@ -6,12 +6,12 @@ dist_tol  = 360/3600*3.141592653/180.0 ;
 
 [jj, sign] = compare2(1, 3, ij, tolerance, m_star, BLI, NEWBLI);% 3rd star search 
 if(sign ~= 2)
-    [NEWBLI, sign] = compare3(2, 4, jj, sign, tolerance, m_star, BLI, NEWBLI);
+    [NEWBLI, sign] = compare3(2, 3, jj, sign, tolerance, m_star, BLI, NEWBLI);
 end
 while(sign == 2  && jj < ij)
     [jj, sign] = compare4(1, 3, jj, ij, m_star, BLI, NEWBLI);
     if(sign ~= 2) 
-        [NEWBLI, sign] = compare3(2, 4, jj, sign, tolerance, m_star, BLI, NEWBLI);
+        [NEWBLI, sign] = compare3(2, 3, jj, sign, tolerance, m_star, BLI, NEWBLI);
     end
 end
 if(sign ~= 2)
